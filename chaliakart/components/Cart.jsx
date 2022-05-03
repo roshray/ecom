edf48988bc7a4c6496166e74f0b9b5b312ce5ad5
roshray <roshanroy759@gmail.com>
2022-05-03 22:39:43 +0530

    subtotal: cart items

--- chaliakart/components/Cart.jsx
+++ chaliakart/components/Cart.jsx
@@ -10,7 +10,7 @@ import { urlFor } from '../lib/client'
 
 const Cart = () => {
   const cartRef = useRef()
-  const {totalPrice, totalQuantities, cartItems, setShowCart} = useStateContext()
+  const {totalPrice, totalQuantities, cartItems, setShowCart, toggleCartItemQuantity} = useStateContext()
 
   return (
     <div className='cart-wrapper' ref={cartRef}>
@@ -50,13 +50,13 @@ const Cart = () => {
                 <div className="flex bottom">
                   <div>
                      <p className="quantity-desc">
-                        <span className="minus" onClick="">
+                        <span className="minus" onClick={() => toggleCartItemQuantity(item._id, 'dec')}>
                             <AiOutlineMinus />
                         </span>
                         <span className="num" onClick=''>
-                            0
+                            {item.quantity}
                         </span>
-                        <span className="plus" onClick="">
+                        <span className="plus" onClick={() => toggleCartItemQuantity(item._id, 'inc')}>
                             <AiOutlinePlus />
                         </span>
                     </p>                   
@@ -74,6 +74,17 @@ const Cart = () => {
             </div>
           ))}
         </div>
+        {cartItems.length >= 1 && (
+          <div className='cart-bottom'>
+            <div className="total">
+              <h3>Subtotal:</h3>
+              <h3>${totalPrice}</h3>
+            </div>
+            <div className="btn-container">
+              <button type="button" className='btn' onClick=''>Pay With Stripe</button>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   )
